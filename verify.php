1871c1871e574906986af2f4bf8c69fb9ed223c2
deadline-cxn <deadline@cityxen.net>
2024-02-26 04:39:16 -0500

    verify.php

---
+++ verify.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+if(isset($_SESSION["LOGGED_IN"])) $USER_DATA=get_user_data($_SESSION["LOGGED_IN"]);
+if(isset($_REQUEST["other_profile"])) $USER_DATA=get_user_data($_REQUEST["other_profile"]);
+
+// dump_vars($_REQUEST);
+
+echo "<h1>VERIFY EMAIL!</h1>";
+
+$v=$_REQUEST["verify"];
+$n=$_REQUEST["name"];
+
+echo "[$v]<br>";
+
+if($v=="true"){
+    echo "YOU ARE NOW VERIFIED!<br>";
+    $ud=get_user_data($n);
+
+    // dump_vars($ud);
+    $ud["verified"]=$v;
+    set_user_data($n,$ud);
+    
+}
+
